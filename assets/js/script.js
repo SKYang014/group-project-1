80534e5c00f0a5cf2ad7e55c8aa7d83b71b91f56
poofy <skyang014@gmail.com>
2022-05-12 20:09:14 -0700

    edited HTML to fit sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //pull elements from html
 var nameEl = document.querySelector("#name");
 var divEl = document.querySelector("#info");
+var contentEl = document.querySelector("#content-info");
 var dogApi = "https://dog.ceo/api/breeds/image/random";
 var infoApi = "https://randomuser.me/api/";
 
@@ -15,30 +16,44 @@ fetch(infoApi)
     return response.json();
 })
 .then (function(data) {
-    console.log(data.results[0].name.title);
-    console.log(data.results[0].name.first);
-    console.log(data.results[0].name.last);
+    
+    var nameTitle = data.results[0].name.title;
+    console.log(nameTitle);
+    var nameTitleEl = document.createElement("h2");
+    nameTitleEl.innerHTML = nameTitle;
+    //nameTitleEl.appendChild(nameTitle);
+    
+    // console.log(data.results[0].name.first);
+    // var nameFirst = data.results[0].name.first;
+    // var nameFirstEl = document.createElement("li");
+    // nameFirstEl.appendChild(nameFirst);
+
+    // console.log(data.results[0].name.last);
+    // var nameLast = data.results[0].name.last;
+    // var nameLastEl = document.createElement("li");
+    // nameLastEl.appendChild(nameLast);
+
     console.log(data.results[0].location.state);
     console.log(data.results[0].location.country);
     console.log("@"+data.results[0].login.username);
     console.log(data.results[0].dob.age+" Dog Years Old");
-
+    nameEl.appendChild(nameTitleEl);
 });
 
 //
 
 
 // insert dog photo function
-fetch(dogApi)
-.then(function(response) {
-    return response.json();
-})
-.then (function(data) {
-    console.log(data);
-    var photoUrl = []
-    var photoEl = document.createElement("img.src = " + data.message + ";");
+// fetch(dogApi)
+// .then(function(response) {
+//     return response.json();
+// })
+// .then (function(data) {
+//     console.log(data);
+//     var photoUrl = []
+//     var photoEl = document.createElement("img.src = " + data.message + ";");
     
-});
+// });
 
 
 
